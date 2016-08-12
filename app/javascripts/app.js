cd48ea7302d4004acb8d44918e6aeb015490c2b1
Dan Finlay <dan@danfinlay.com>
2016-08-11 17:43:51 -0700

    Made in the way I would do it with metamask

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -6,6 +6,12 @@ var user = User.deployed();
 var pid;
 var customers= new Array();
 
+function startApp() {
+  var account = web3.eth.accounts[0]
+  var acctEl = document.getElementById("currentAccount");
+  acctEl.innerText = account
+}
+
 function setStatus(message) {
   var status = document.getElementById("status");
   status.innerHTML = message;
@@ -20,28 +26,36 @@ function setStatus1(message) {
 function buyProd() {
 
 
-  var account = document.getElementById('buyAcc').value;
+  var account = web3.eth.accounts[0]
+  console.log("account: " + account);
+
   var contAddress = "0x69eabf45623e5b53683924dff98309eb39af1a89";
   var etherValue = 2;
-  var account1 = account.toLowerCase();
-  console.log("account1: " + account1);
 
     //Send Ether to Contract
-      web3.eth.sendTransaction({from:account1,to: contAddress, value: etherValue}, function (err, value1) {
+      web3.eth.sendTransaction({from:account,to: contAddress, value: etherValue}, function (err, value1) {
        if (err != null) {
          alert ("There was an error fetching contract Balance in Send Ether");
          console.log("Send Ether error: " + err);
        } else {
          setStatus("Ether Sent Successfully.")
-    }
+      }
     });
-    //Send Ether to Contract
-
 
 };
 
-window.onload = function() {
+window.addEventListener('load', function() {
 
+  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+  if (typeof web3 !== 'undefined') {
 
+    //
+    web3 = new Web3(web3.currentProvider);
+  } else {
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  }
 
-};
+  // Then you can now safely start your app & access web3 freely:
+  startApp()
+})
